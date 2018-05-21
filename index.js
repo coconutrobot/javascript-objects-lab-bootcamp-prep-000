d2247e5d544e1d217ac3299374b9937188881b9e
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-21 03:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function deleteFromObjectByKey(recipes, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(recipes, key) {
-  delete recipes.key;
+  delete recipes[key];
   return recipes;
 }
