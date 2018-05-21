333dc6c1ee70f5c398fa3049b220a94812cbdc12
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-21 03:30:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
   
-  return Object.assign({}, recipes, {})
+  return Object.assign({}, recipes, {[key]: value})
   
   var obj = {prop:1};
   recipes[key] = value;
