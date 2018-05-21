03f5b1c4d3792775848aa497a80e3393def63133
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-21 03:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
   
-  return Object.assign({}, recipes, {[key]: value})
-  
-  var obj = {prop:1};
-  recipes[key] = value;
-  
-  return recipes;
+  return Object.assign({}, recipes, {[key]: value});
 }
